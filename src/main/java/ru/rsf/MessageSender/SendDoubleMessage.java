ac45deeacda3f7b78f4ce265ef334ea67f1dcca4
no8ter <76181221+no8ter@users.noreply.github.com>
2021-07-29 08:41:49 +0300

    Update SendDoubleMessage.java

--- src/main/java/ru/rsf/MessageSender/SendDoubleMessage.java
+++ src/main/java/ru/rsf/MessageSender/SendDoubleMessage.java
@@ -2,14 +2,21 @@ package ru.rsf.MessageSender;
 
 public class SendDoubleMessage implements Command{
 
+    String smsTitle;
+    String smsMessage;
+    String emailTitle;
+    String emailMessage;
     SendDoubleMessage(String smsTitle, String smsMessage, String emailTitle, String emailMessage) {
-        
+        this.smsTitle = smsTitle;
+        this.smsMessage = smsMessage;
+        this.emailMessage = emailMessage;
+        this.emailTitle = emailTitle;
     }
 
     @Override
     public void execute(){
-        Command sms = new SendSMS("", "");
-        Command email = new SendEmail("", "");
+        Command sms = new SendSMS(smsTitle, smsMessage);
+        Command email = new SendEmail(emailTitle, emailMessage);
         sms.execute();
         email.execute();
     }
